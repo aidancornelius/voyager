620fda9e02418ad47c419fc85ac3967fbab88958
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2017-07-30 10:58:54 +0930

    Cleaned up the messages system

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,10 +4,8 @@ class Message < ApplicationRecord
   after_create :notify_and_upsert
 
   def notify_and_upsert
-      if self.user.present?
-         if self.user.email_preference != false
-           NotificationMailer.notification(self.user.email, self.title, "https://naturalmaths.com.au/messages").deliver
-         end
-       end
+    if self.user.present?
+       NotificationMailer.notification(self.user.email, self.title, "https://naturalmaths.com.au/messages").deliver
+     end
   end
 end
