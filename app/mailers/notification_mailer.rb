ccb49781c5594ea2a1ef6e172319a9d855741459
Aidan Cornelius-Bell <aidancornelius@aidans-imac-pro.lan>
2019-01-30 10:23:27 +1030

    Messages and mailer updates

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,6 @@ class NotificationMailer < ApplicationMailer
   def notification(email, title, link)
     @title = title
     @link = link
-    mail( to: email )
+    mail( to: email, bcc: "annbaker111@gmail.com, aidan@teachersolutions.com.au" )
   end
 end
