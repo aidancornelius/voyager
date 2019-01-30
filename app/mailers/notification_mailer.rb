1ddada284727041104b7ae724be03f28f8aa5c42
Aidan Cornelius-Bell <aidan@teachersolutions.com.au>
2019-01-30 10:46:37 +1030

    Built a mailer for all new comments.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,6 +2,12 @@ class NotificationMailer < ApplicationMailer
   def notification(email, title, link)
     @title = title
     @link = link
-    mail( to: email, bcc: "annbaker111@gmail.com, aidan@teachersolutions.com.au" )
+    mail( to: email )
+  end
+
+  def new_message(email, user, link)
+    @user = user
+    @link = link
+    mail ( to: email )
   end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -19,6 +19,10 @@ class Reply < ApplicationRecord
         color: '#536531'
     }
     ac.notify_slack('there is a new reply at [Natural Maths](https://naturalmaths.com.au/replies).', attachments)
+    # Here we need ot go on a bit of a goose chase to build the URL for this reply:
+    lesson_component_for_reply = LessonComponent.find(self.lesson_component_id)
+    urlbuild = "/courses/#{lesson_component_for_reply.lesson.course.slug}/modules/#{lesson_component_for_reply.lesson.slug}/#{lesson_component_for_reply.slug}"
+    NotificationMailer.new_message("aidan@teachersolutions.com.au", self.user, urlbuild)
   end
 
   def thread_id
